08ba148de4dfe3c4924686e2d7b5a15a3938d3d9
Miryam <Miryam.Schwartz@mail.huji.ac.il>
2023-03-29 17:11:18 +0300

    ex2 - until 2.3, need clean

--- VAN_ex/code/ex2.py
+++ VAN_ex/code/ex2.py
@@ -1,8 +1,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import  cv2 as cv
+import cv2 as cv
+import plotly.express as px
+import plotly.graph_objs as go
 
 import utils
+
+THRESHOLD = 1
 
 def deviation_single_match(match, kp1, kp2):
     return abs(kp1[match.queryIdx].pt[1] - kp2[match.trainIdx].pt[1])
@@ -20,7 +24,32 @@ def rectified_stereo_pattern_test(kp1, kp2, matches, threshold):
     return img1_inliers, img1_outliers, img2_inliers, img2_outliers
 
 
-def triangulation(p, q, p_cam_mat, q_cam_mat):   # p_cam_mat -> left camera, img1
+def triangulation(kp1, kp2, triangulation_func):
+    k, m1, m2 = utils.read_cameras()
+    dots_3d = []
+    for i in range(len(kp1)):
+        p = kp1[i].pt
+        q = kp2[i].pt
+        ret_pt_4d = triangulation_func(k@m1, k@m2, p, q)
+        ret_pt_3d = ret_pt_4d[:3] / ret_pt_4d[3]
+        dots_3d.append(ret_pt_3d)
+    shape_cv = np.array(dots_3d).shape
+    dots_3d = np.array(dots_3d).reshape(shape_cv[:2])
+    return dots_3d
+
+def show_dots_3d_cloud(dots_3d, output_name):
+    dots_3d = np.array(dots_3d)
+    x = dots_3d[:, 0].reshape(-1)
+    y = dots_3d[:, 1].reshape(-1)
+    z = dots_3d[:, 2].reshape(-1)
+    trace = go.Scatter3d(x=x, y=y, z=z, mode='markers', marker=dict(size=3), name='objects')
+    fig = go.Figure(data=[trace])
+    fig.add_trace(go.Scatter3d(x=[0], y=[0], z=[0], mode='markers', marker=dict(size=5, color='red'), name='camera'))
+    fig.show()
+    fig.write_html(output_name)
+
+
+def triangulation_single_match(p_cam_mat, q_cam_mat, p, q):   # p_cam_mat -> left camera, img1
     p_x = p[0]
     p_y = p[1]
     q_x = q[0]
@@ -31,9 +60,29 @@ def triangulation(p, q, p_cam_mat, q_cam_mat):   # p_cam_mat -> left camera, img
     a_mat[2] = q_x * q_cam_mat[2] - q_cam_mat[0]
     a_mat[3] = q_y * q_cam_mat[2] - q_cam_mat[1]
     U, D, V_t = np.linalg.svd(a_mat)
-    return V_t[3][:3]
+    return V_t[3]
 
+def median_distance_between_3d_points(lst_pt1, lst_pt2):
+    dist = np.linalg.norm(np.array(lst_pt2)-np.array(lst_pt1), axis=1)
+    return np.median(dist)
 
+def get_correlated_kps_from_matched(kp1, kp2, matches):
+    kp1_ret, kp2_ret = [], []
+    for i, match in enumerate(matches):
+        kp1_ret.append(kp1[match.queryIdx])
+        kp2_ret.append(kp2[match.trainIdx])
+    return kp1_ret, kp2_ret
+
+def match_and_triangulate_image(idx, triangulation_func, rectified_test=False):
+    img1, img2 = utils.read_images(0)
+    kp1, des1, kp2, des2 = utils.detect_and_compute(img1, img2)
+    matches = utils.find_matches(img1, kp1, des1, img2, kp2, des2)
+    if rectified_test:
+        kp1_matched, _, kp2_matched, _ = rectified_stereo_pattern_test(kp1, kp2, matches, THRESHOLD)
+    else:
+        kp1_matched, kp2_matched = get_correlated_kps_from_matched(kp1, kp2, matches)
+    dots_3d = triangulation(kp1_matched, kp2_matched, triangulation_func)
+    show_dots_3d_cloud(dots_3d, f"3D_points_cloud_{idx}.html")
 
 def ex2_run():
 
@@ -54,7 +103,7 @@ def ex2_run():
     print("Percentage of matches that deviate by more than two pixels: ", np.count_nonzero(deviation > 2) / len(matches) * 100)
 
     # 2.2
-    img1_inliers, img1_outliers, img2_inliers, img2_outliers = rectified_stereo_pattern_test(kp1, kp2, matches, 1)
+    img1_inliers, img1_outliers, img2_inliers, img2_outliers = rectified_stereo_pattern_test(kp1, kp2, matches, THRESHOLD)
     img1 = cv.drawKeypoints(img1, img1_inliers, img1, color=(255, 140, 0))
     img1 = cv.drawKeypoints(img1, img1_outliers, img1, color=(0, 255, 255))
     cv.imwrite('rectified_stereo_pattern_img1.png', img1)
@@ -65,12 +114,18 @@ def ex2_run():
     cv.imwrite('rectified_stereo_pattern_img2.png', img2)
     plt.imshow(img2), plt.show()
 
-    p = kp1[matches[0].queryIdx].pt
-    q = kp2[matches[0].trainIdx].pt
+    #2.3
+    #kp1_matched, kp2_matched = get_correlated_kps_from_matched(kp1, kp2, matches)
+    #dots_3d_us = triangulation(kp1_matched, kp2_matched, triangulation_single_match)
+    #dots_3d_cv = triangulation(kp1_matched, kp2_matched, cv.triangulatePoints)
+    #print(len(dots_3d_us), len(dots_3d_cv))
+    #show_dots_3d_cloud(dots_3d_us, "3D_points_cloud_us.html")
+    #show_dots_3d_cloud(dots_3d_cv, "3D_points_cloud_cv.html")
+    #median = median_distance_between_3d_points(dots_3d_cv, dots_3d_us)
+    #print(median)
+    match_and_triangulate_image(0, triangulation_single_match)
+    #2.4
 
-    k, m1, m2 = utils.read_cameras()
-    ret = triangulation(p, q, m1, m2)
-    print(ret)
 
 
 if __name__ == '__main__':
