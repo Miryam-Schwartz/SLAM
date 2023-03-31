eb7ffc6ad0bc2c802d4fd9aed612a0fa66b90af3
Miryam <Miryam.Schwartz@mail.huji.ac.il>
2023-03-31 14:36:58 +0300

    ex2, finish documentation

--- VAN_ex/code/ex2.py
+++ VAN_ex/code/ex2.py
@@ -14,6 +14,15 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 
 def get_correlated_kps_from_matches(kp1, kp2, matches):
+    """
+    Return matched key-points.
+    :param kp1: list of key-points of left image
+    :param kp2: list of key-points of right image
+    :param matches: list of matches between right and left image
+    :return: 2 lists of key-points, for right and left left images,
+    when, i keypoint in kp1, corresponding to i keypoint in kp2.
+    Elements type of kp1 and kp2 is KeyPoint.
+    """
     kp1_ret, kp2_ret = [], []
     for i, match in enumerate(matches):
         kp1_ret.append(kp1[match.queryIdx])
@@ -21,14 +30,32 @@ def get_correlated_kps_from_matches(kp1, kp2, matches):
     return kp1_ret, kp2_ret
 
 
+def difference_y_axis_between_two_kps(single_kp1, single_kp2):
+    """
+    Calculate the difference in y-axis between two key-points
+    :param single_kp1: single KeyPoint instance
+    :param single_kp2: also
+    :return: difference in y-axis between two key-points
+    """
+    return abs(single_kp1.pt[1] - single_kp2.pt[1])
+
+
 def deviation_from_pattern(kp1, kp2):
-    deviation = np.zeros(len(kp1))
-    for i in range(len(kp1)):
-        deviation[i] = abs(kp1[i].pt[1] - kp2[i].pt[1])
-    return deviation
+    """
+    Calculate deviation in y-axis for list of points.
+    :param kp1: list of key-points
+    :param kp2: list of key-points
+    :return: list of the differences in y-axis.
+    The deviation from kp1[i] to kp2[i] is located in index i in return list.
+    """
+    return np.array([difference_y_axis_between_two_kps(kp1[i], kp2[i]) for i in range(len(kp1))])
 
 
 def create_deviation_hist(deviation):
+    """
+    Save and show histogram of deviations
+    :param deviation: list of deviations
+    """
     plt.hist(deviation, 25)
     plt.xlabel("Deviation from rectified stereo pattern")
     plt.ylabel("Number of Matches")
@@ -36,14 +63,18 @@ def create_deviation_hist(deviation):
     plt.show()
 
 
-def deviation_single_match(single_kp1, single_kp2):
-    return abs(single_kp1.pt[1] - single_kp2.pt[1])
-
-
 def rectified_stereo_pattern_test(kp1, kp2, threshold):
+    """
+    Reject matches according to stereo pattern test: if the deviation in y-axis,
+    between right and left image is more than threshold, reject the points.
+    :param kp1: list of KeyPints, matched to kp2
+    :param kp2:
+    :param threshold:
+    :return: img1_inliers, img1_outliers, img2_inliers, img2_outliers
+    """
     img1_inliers, img1_outliers, img2_inliers, img2_outliers = [], [], [], []
     for i in range(len(kp1)):
-        if deviation_single_match(kp1[i], kp2[i]) < threshold:
+        if difference_y_axis_between_two_kps(kp1[i], kp2[i]) < threshold:
             img1_inliers.append(kp1[i])
             img2_inliers.append(kp2[i])
         else:
@@ -53,6 +84,14 @@ def rectified_stereo_pattern_test(kp1, kp2, threshold):
 
 
 def show_accepted_and_rejected_points_on_img(img, inliers_pts, outliers_pts, img_num):
+    """
+    Show and save the accepted and rejected points on the image.
+    Inlier points in orange, and outliers in cyan.
+    :param img: original image
+    :param inliers_pts: list of inliers key-points
+    :param outliers_pts: list of outliers key-points
+    :param img_num: 1 for left, 2 for right
+    """
     img = cv.drawKeypoints(img, inliers_pts, img, color=ORANGE_COLOR)
     img = cv.drawKeypoints(img, outliers_pts, img, color=CYAN_COLOR)
     cv.imwrite(f'{OUTPUT_DIR}rectified_stereo_pattern_img{img_num}.png', img)
@@ -60,17 +99,33 @@ def show_accepted_and_rejected_points_on_img(img, inliers_pts, outliers_pts, img
 
 
 def triangulation_single_match(p_cam_mat, q_cam_mat, p, q):  # p_cam_mat -> left camera, img1
+    """
+    Triangulate a point - convert from picture coordinates of two matched points, to world coordinates.
+    :param p_cam_mat: left camera matrix
+    :param q_cam_mat: right camera matrix
+    :param p: point on image 1, represented by x and y coordinates of pixels
+    :param q: point on image 2, represented by x and y coordinates of pixels
+    :return: 3d world coordinates of the point
+    """
     p_x, p_y = p[0], p[1]
     q_x, q_y = q[0], q[1]
     a_mat = np.array([p_x * p_cam_mat[2] - p_cam_mat[0],
                       p_y * p_cam_mat[2] - p_cam_mat[1],
                       q_x * q_cam_mat[2] - q_cam_mat[0],
                       q_y * q_cam_mat[2] - q_cam_mat[1]])
-    U, D, V_t = np.linalg.svd(a_mat)
-    return V_t[3]
+    _, _, v_t = np.linalg.svd(a_mat)
+    return v_t[3]
 
 
-def triangulation(kp1, kp2, triangulation_func):
+def triangulation_list_of_points(kp1, kp2, triangulation_func):
+    """
+    Triangulation for lists of points from right and left images.
+    :param kp1: list of key-points, matched to kp2
+    :param kp2:
+    :param triangulation_func: function that makes the triangulation.
+    Can be OpenCv.triangulatePoints or our implementation - triangulation_single_match
+    :return: list of 3d points
+    """
     k, m1, m2 = utils.read_cameras()
     dots_3d = []
     for i in range(len(kp1)):
@@ -85,6 +140,12 @@ def triangulation(kp1, kp2, triangulation_func):
 
 
 def show_dots_3d_cloud(dots_3d, output_name, show=False):
+    """
+    Create interactive 3d points cloud
+    :param dots_3d: list of 3d points
+    :param output_name: output name of the file that is created
+    :param show: if true - show and save the file, else: only save/
+    """
     dots_3d = np.array(dots_3d)
     x = dots_3d[:, 0].reshape(-1)
     y = dots_3d[:, 1].reshape(-1)
@@ -98,18 +159,31 @@ def show_dots_3d_cloud(dots_3d, output_name, show=False):
 
 
 def median_distance_between_3d_points(lst_pt1, lst_pt2):
+    """
+    Calculate the median distance between two lists of 2d points
+    :param lst_pt1:
+    :param lst_pt2:
+    :return: median distance
+    """
     dist = np.linalg.norm(np.array(lst_pt2) - np.array(lst_pt1), axis=1)
     return np.median(dist)
 
 
 def match_and_triangulate_image(im_idx, triangulation_func, rectified_test=False):
-    img1, img2 = utils.read_images(0)
+    """
+    Read, find key-points, match and triangulate points.
+    :param im_idx: index pair images
+    :param triangulation_func: function that makes the triangulation.
+    :param rectified_test: if True, reject points according to rectified_test
+    :return: list of 3d points, after the triangulation
+    """
+    img1, img2 = utils.read_images(im_idx)
     kp1, des1, kp2, des2 = utils.detect_and_compute(img1, img2)
     matches = utils.find_matches(img1, kp1, des1, img2, kp2, des2)
     kp1, kp2 = get_correlated_kps_from_matches(kp1, kp2, matches)
     if rectified_test:
         kp1, _, kp2, _ = rectified_stereo_pattern_test(kp1, kp2, THRESHOLD)
-    dots_3d = triangulation(kp1, kp2, triangulation_func)
+    dots_3d = triangulation_list_of_points(kp1, kp2, triangulation_func)
     return dots_3d
 
 
@@ -137,19 +211,19 @@ def ex2_run():
     print("Number of matches were discarded: ", len(img1_outliers))
 
     # 2.3 linear least squares triangulation
-    our_dots_3d = triangulation(kp1, kp2, triangulation_single_match)
+    our_dots_3d = triangulation_list_of_points(kp1, kp2, triangulation_single_match)
 
     # present a 3d plot of the calculated 3d points
     show_dots_3d_cloud(our_dots_3d, f"{OUTPUT_DIR}our_3D_points_cloud_img0.html")
 
     # repeat the triangulation using ‘cv2.triangulatePoints’
-    cv_dots_3d = triangulation(kp1, kp2, cv.triangulatePoints)
+    cv_dots_3d = triangulation_list_of_points(kp1, kp2, cv.triangulatePoints)
     show_dots_3d_cloud(our_dots_3d, f"{OUTPUT_DIR}cv_3D_points_cloud_img0.html")
 
     # print the median distance between the corresponding 3d points in our implementation,
     # compare to opencv implementation
     median = median_distance_between_3d_points(cv_dots_3d, our_dots_3d)
-    print("Median distance between corresponding 3d points in our implementation,"
+    print("Median distance between corresponding 3d points in our implementation, "
           "compare to opencv implementation: ", median)
 
     # 2.4 Run this process (matching and triangulation) over a few pairs of images
